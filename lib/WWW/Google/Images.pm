9dc279559e20a86fb36a3ffb02f377f1a089d7e3
rousse <rousse>
2005-01-31 15:34:00 +0000

    fixed text matching regexp

--- lib/WWW/Google/Images.pm
+++ lib/WWW/Google/Images.pm
@@ -187,7 +187,7 @@ sub _extract_images {
 	my $parser = HTML::Parser->new();
 	my $callback = sub {
 	    my ($text) = @_;
-	    if ($text =~ /^(\d+) x (\d+) pixels - (\d+) ko$/) {
+	    if ($text =~ /^(\d+) x (\d+) pixels - (\d+)k$/) {
 		push(@data, { width => $1, height => $2, size => $3 });
 	    }
 	};
